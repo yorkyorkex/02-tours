bb4068a1003056443035d2c284da3dc280db8103
York Yu <n11453559@gmail.com>
2025-09-08 21:36:26 +1000

    Add read more functionality to Tour component for improved user experience

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tour = ({ id, image, info, price, name, removeTour }) => {
+  const [readMore, setReadMore] = useState(false)
+
   return (
     <article key={id} className="single-tour">
       <img src={image} alt={name} className="img" />
       <span className="tour-price">${price}</span>
       <div className="tour-info">
         <h5>{name}</h5>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            type="button"
+            onClick={() => setReadMore(!readMore)}
+            className="info-btn"
+          >
+            {readMore ? 'Show Less' : 'Read More'}
+          </button>
+        </p>
         <div className="tour-footer">
           <button
             type="button"
